5fae43ba8a4361f00e9bf3872c7aa2a7538eb8aa
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2025-12-31 00:51:11 +0200

    adding login

--- Src/components/auth/login/login.js
+++ Src/components/auth/login/login.js
@@ -4,9 +4,35 @@ const loginForm = document.getElementById("loginform");
 // login
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const rememberMe = document.getElementById("remember").checked;
+
+    if (email === "" || password === "") {
+        alert("All fields are required!");
+        return;
+    }
+
+    let isExist = await isUserExist(email);
+    if (!isExist) {
+        alert("Email address or Password is not correct!");
+        return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+    let isPasswordCorrect = await checkPassword(email, hashedPassword);
+    if (!isPasswordCorrect) {
+        alert("Email address or Password is not correct!");
+        return;
+    }
+
+    saveUserInSession(email, hashedPassword);
+    let noDays = 1;
+    if (rememberMe)
+        noDays = 7;
+
+    saveUserInCookies(email, hashedPassword, noDays);
+
     alert("Login successful!");
+    redirectTo("../../../../index.html");
 });
-
-
